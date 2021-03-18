740147a496abe91d370f563b972ba734001311a1
johnmathews <mthwsjc@gmail.com>
2021-03-18 11:41:05 +0100

    add -y flag to apt-get install commands

--- debian-10.sh
+++ debian-10.sh
@@ -1,15 +1,15 @@
 # install utilities
 sudo apt-get update
-sudo apt install -y git git-lfs htop atop mc wget zip jq python3-pip python docker
+sudo apt-get install -y git git-lfs htop atop mc wget zip jq python3-pip python docker
 
 sudo apt-get update
-sudo apt-get install apt-transport-https ca-certificates curl gnupg lsb-release
+sudo apt-get install -y apt-transport-https ca-certificates curl gnupg lsb-release
 
 # install docker
 curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get update
-sudo apt-get install docker-ce docker-ce-cli containerd.io
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
 # dont prepend docker commands with sudo
 sudo groupadd docker
